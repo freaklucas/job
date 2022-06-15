a5add3b966db5e635bf29675d4213ec0007f59ee
freaklucas <lucas.oliveira.freak@gmail.com>
2022-06-14 23:04:40 -0300

    conexão com bd ok

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const database = require("./db/connection");
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Express está rodando na porta ${PORT}`);
+  console.log(`Express executing ok in port ${PORT}`);
 });
+
+database
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.log(`erro in connection the bd ${err}`);
+  });
 
 app.get("/", (req, res) => {
   res.send(`Está funcionando na porta ${PORT}`);
