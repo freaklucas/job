ba9f8b98b464a576cf15256bd69f6f2e3596505e
freaklucas <lucas.oliveira.freak@gmail.com>
2022-06-16 00:02:30 -0300

    rota de add modelada

--- routes/jobs.js
+++ routes/jobs.js
@@ -4,11 +4,15 @@ const Job = require("../models/Job");
 
 // add job
 router.post("/add", (req, res) => {
-  Job.create(req.body)
-    .then((job) => {
-      res.json(job);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
+  let { title, description, salary, company, email } = req.body;
+
+  Job.create({
+    title,
+    description,
+    salary,
+    company,
+    email,
+  })
+    .then(() => res.redirect("/"))
+    .catch((err) => console.log(err));
 });
