e240f6561e95c7dac521e46eb83b2e6071d38e58
freaklucas <lucas.oliveira.freak@gmail.com>
2022-06-15 23:58:54 -0300

    config bodyparser

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const database = require("./db/connection");
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,6 +8,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Express executing ok in port ${PORT}`);
 });
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 database
   .authenticate()
