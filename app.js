8ecf2d295037ec8cecad5df17438f6ee37ed6258
freaklucas <lucas.oliveira.freak@gmail.com>
2022-06-19 01:14:53 -0300

    add handlebars

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+
 const database = require("./db/connection");
 const bodyParser = require("body-parser");
 
 const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Express executing ok in port ${PORT}`);
 });
 
+// bodyparser
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//handlebars
+app.set("view engine", path.join(__dirname, "views"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// db connection
 database
   .authenticate()
   .then(() => {
@@ -20,6 +30,7 @@ database
     console.log(`erro in connection the bd ${err}`);
   });
 
+// routes
 app.get("/", (req, res) => {
   res.send(`Está funcionando na porta ${PORT}`);
 });
